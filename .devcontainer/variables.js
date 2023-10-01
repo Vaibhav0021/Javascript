d1ce8ccc677749ae37dea90ae1e8f811270b2245
Vaibhav0021 <129607340+Vaibhav0021@users.noreply.github.com>
2023-10-01 15:22:34 +0530

    Update and rename test.js to variables.js

---
+++ .devcontainer/variables.js
@@ -0,0 +1,8 @@
+// console.log("hello")
+const accountName="vaibhav"
+let accountCity;
+let accountState="jaipur"
+console.log(accountState)
+accountState="bengal"
+console.log(accountState)
+console.table([accountName,accountCity,accountState])
